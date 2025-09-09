cc62c3c503efd7a66648ebbdbffe040c55f0970b
Mudi540 <muhammdmukhtar20@gmail.com>
2025-09-10 00:31:56 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,19 +2,19 @@ const socket = io();
 const video = document.getElementById("videoPlayer");
 const uploadForm = document.getElementById("uploadForm");
 
-uploadForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const formData = new FormData(uploadForm);
-  const res = await fetch("/upload", { method: "POST", body: formData });
-  const data = await res.json();
-  video.src = data.file;
+socket.on("videoUploaded", (url) => {
+  video.src = url;
+  video.load();
   video.play();
 });
 
+// Controls
 video.addEventListener("play", () => socket.emit("play"));
 video.addEventListener("pause", () => socket.emit("pause"));
 video.addEventListener("seeked", () => socket.emit("seek", video.currentTime));
 
 socket.on("play", () => video.play());
 socket.on("pause", () => video.pause());
-socket.on("seek", (time) => { video.currentTime = time; });
+socket.on("seek", (time) => {
+  video.currentTime = time;
+});
